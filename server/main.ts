a6f5f5b28c079295716eaae65cbc898b946048e3
ximing <807754634@qq.com>
2019-05-26 01:03:27 +0800

    feat: 修改ts配置

--- server/main.ts
+++ server/main.ts
@@ -1,40 +1,40 @@
-import * as Koa from "koa";
-import mongodb from "./utils/mongodb";
-import { getEnvironment } from "./utils/config";
-import exception from "./middleware/exception";
-import routers from "./routers/index";
-import service from "./services/index";
+import * as Koa from 'koa';
+import mongodb from './utils/mongodb';
+import { getEnvironment } from './utils/config';
+import exception from './middleware/exception';
+import routers from './routers/index';
+import service from './services/index';
 
-const http = require("http");
-const logger = require("koa-logger");
-const koaJson = require("koa-json");
-const bodyParser = require("koa-bodyparser");
+const http = require('http');
+const logger = require('koa-logger');
+const koaJson = require('koa-json');
+const bodyParser = require('koa-bodyparser');
 
 let port = 8080;
 const app = new Koa();
 app.use(exception());
 app.use(bodyParser());
 app.use(
-  koaJson({
-    pretty: false,
-    param: "pretty"
-  })
+    koaJson({
+        pretty: false,
+        param: 'pretty',
+    }),
 );
 app.use(logger());
 const server = http.createServer(app.callback());
 routers(app);
 mongodb.init().then(async ({ mongoDB }) => {
-  try {
-    console.log("connect kafka success");
-    server.listen(port, () => {
-      console.log("启动服务成功，监听端口", port);
-      app.context.mongoDB = mongoDB;
-      service(app);
-      // const apolloServer = graphql(app);
-      // console.log('graph path is', apolloServer.graphqlPath);
-    });
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
+    try {
+        console.log('connect kafka success');
+        server.listen(port, () => {
+            console.log('启动服务成功，监听端口', port);
+            app.context.mongoDB = mongoDB;
+            service(app);
+            // const apolloServer = graphql(app);
+            // console.log('graph path is', apolloServer.graphqlPath);
+        });
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
 });
